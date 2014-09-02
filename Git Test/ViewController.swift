d18c07aa7c8ee02437dbf6a660b5a239953799f8
Scott Galbraith <todd@Scotts-MacBook-Pro.local>
2014-09-02 17:45:54 -0600

    Initial Commit

---
+++ Git Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git Test
+//
+//  Created by Scott Galbraith on 9/2/14.
+//  Copyright (c) 2014 Great American Insurance Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
