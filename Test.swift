d70d411ce560bc9555bbbc0fa0065a23cc76d4e4
sgalbraith <oz@actionpov.com>
2014-09-02 18:47:12 -0600

    added a test class

---
+++ Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  Git Test
+//
+//  Created by Scott Galbraith on 9/2/14.
+//  Copyright (c) 2014 Great American Insurance Group. All rights reserved.
+//
+
+import UIKit
+
+class Test: NSObject {
+   
+}
